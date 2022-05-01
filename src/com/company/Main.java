2dc7bbedb97da9c3d745c6fe489ad38aed45ae61
azeez1776 <aboud3am81@gmail.com>
2022-05-01 15:35:48 +0300

    :construction: chore: calculated the final position after falling for 10 secs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,23 @@ package com.company;
 
 public class Main {
 
+    public static double computePosition(double gravity, double fallingTime, double initialVelocity, double initialPosition){
+        double finalPosition;
+        finalPosition = 0.5 * gravity * fallingTime * fallingTime  + initialVelocity * fallingTime + initialPosition;
+        return finalPosition;
+    };
+
     public static void main(String[] args) {
         double gravity = -9.81; // Earth's gravity in m/s^2
         double initialVelocity = 0.0;
         double fallingTime = 10.0;
         double initialPosition = 0.0;
         double finalPosition = 0.0;
-        System.out.println("The object's position after " + fallingTime +
+
+        finalPosition = computePosition(gravity, fallingTime, initialVelocity, initialPosition);
+
+        System.out.println("The object's position after " + fallingTime * initialVelocity
+                +
                 " seconds is " + finalPosition + " m.");
     }
 }
